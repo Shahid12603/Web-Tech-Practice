6ceb70d9e5a4c0f3574e93799e2a0a5c34406cf1
Shahid12603 <mohamedshahid.12603@gmail.com>
2025-03-21 15:27:20 +0530

    CSS completed and JS started

---
+++ JAVASCRIPT/String-Functions/string.js
@@ -0,0 +1,41 @@
+let sname  = "Virat";
+
+let upper = sname.toUpperCase();
+
+console.log(sname);
+console.log(upper);
+
+let lname = "KOHLI";
+
+let lower = lname.toLowerCase();
+
+console.log(lname);
+console.log(lower);
+
+console.log(lname.includes("O"));
+
+console.log(lname.charAt(2));
+
+console.log(lname.indexOf("H"));
+
+console.log(lname.lastIndexOf("I"));
+
+let fname = sname.concat(" ", lname, " is ", "Best Batsman......");
+
+console.log(fname);
+
+let trimname = "   Hellooo ";
+console.log(trimname.length);
+
+console.log(trimname.trim().length);
+
+let str = "Hi How are you...?";
+
+let strArr = str.split(" ");
+
+console.log(strArr);
+
+
+
+
+
